801d958cbddf2da8a49aeed1be06254c060af38d
central <central@think.yak.net>
2012-12-09 13:17:03 -0800

    argv includes command name

--- src/terp/cmds.go
+++ src/terp/cmds.go
@@ -8,12 +8,28 @@ import (
 var Builtins map[string]Command = make(map[string]Command, 0)
 
 func (t *Terp) initBuiltins() {
-	Builtins["+"] = cmdPlus
+	Builtins["+"] = MkChainingBinaryFlopCmd(t, BinaryFlop{ return a + b})
 	Builtins["must"] = cmdMust
 	Builtins["-"] = MkBinaryFlopCmd(t, func(a, b float64) float64 { return a - b })
 }
 
 type BinaryFlop func(a, b float64) float64
+
+func MkBinaryFlopCmd(t *Terp, flop BinaryFlop) Command {
+	return func(t *Terp, argv List) Any {
+		a, b := CheckArgs2(argv)
+		return flop(ToFloat(a), ToFloat(b))
+	}
+}
+
+func MkChainingBinaryFlopCmd(t *Terp, z float64, flop BinaryFlop) Command {
+	return func(t *Terp, argv List) Any {
+		for _, a := range argv[1:] {
+			z += ToFloat(a)
+		}
+		return z //Str(z)
+	}
+}
 
 func ToFloat(a Any) float64 {
 	switch x := a.(type) {
@@ -42,22 +58,6 @@ func CheckArgs2(v List) (Any, Any) {
 		panic(Sprintf("Expected 2 argv, but got %#v", v))
 	}
 	return v[1], v[2]
-}
-
-func MkBinaryFlopCmd(t *Terp, flop BinaryFlop) Command {
-	return func(t *Terp, argv List) Any {
-		a, b := CheckArgs2(argv)
-		return flop(ToFloat(a), ToFloat(b))
-	}
-}
-
-func MkChainingBinaryFlopCmd(t *Terp, z float64, flop BinaryFlop) Command {
-	return func(t *Terp, argv List) Any {
-		for _, a := range argv[1:] {
-			z += ToFloat(a)
-		}
-		return z //Str(z)
-	}
 }
 
 func cmdPlus(t *Terp, argv List) Any {
