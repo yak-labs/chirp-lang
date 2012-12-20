811edcf6408812a89c4319316cf08f41553b4b55
Ryan Seto <mr.werewolf@gmail.com>
2012-12-20 09:05:17 -0500

    Modify make.cmd to just make goterp.exe.  We'll run goterp by hand on Windows.

--- make.cmd
+++ make.cmd
@@ -7,10 +7,3 @@ go run src\mkreflections.go > src\generated\reflections.go
 
 :: Make goterp.
 go build -x goterp
-
-:: Put the demo\hello_web.t into a variable for parsing.
-:: (http://stackoverflow.com/questions/3068929)
-for /f "delims=" %%x in (demo\hello_web.t) do set web=%web% %%x
-
-:: Run demo\hello_web.t
-goterp "%web%"
